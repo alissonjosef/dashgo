78d7bbd33eaa5eb6db9f8c2d02af3770ed44331e
Alisson Silva <alissonjosef@gmail.com>
2022-02-15 20:43:47 -0300

    [feat-02] feat: dashbiard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,17 @@
-import Header from "../components/Form/Header"
+import { Flex } from "@chakra-ui/react";
+import Header from "../components/Form/Header";
+import { Sidebar } from "../components/Form/Sidebar";
 
 const dashboard: React.FC = () => {
-    return (
-        <Header />
-    )
-}
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
 
-export default dashboard
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
+  );
+};
+
+export default dashboard;
