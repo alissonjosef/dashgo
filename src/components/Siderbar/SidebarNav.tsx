8c44b84709f70a3c8a6b7cc850de1d30d416c61a
Alisson Silva <alissonjosef@gmail.com>
2022-02-16 23:03:46 -0300

    [feat-04] feat: isActive

--- src/components/Siderbar/SidebarNav.tsx
+++ src/components/Siderbar/SidebarNav.tsx
@@ -7,13 +7,13 @@ export function SidebarNav(){
     return (
         <Stack spacing="12" align="flex-start">
         <NavSection title="GERAL">
-          <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
-          <NavLink icon={RiContactsLine}>Usuarios</NavLink>
+          <NavLink icon={RiDashboardLine} href='/dashboard'>Dashboard</NavLink>
+          <NavLink icon={RiContactsLine} href='/users'>Usuarios</NavLink>
         </NavSection>
 
         <NavSection title="AUTOMAÇÂO">
-          <NavLink icon={RiInputMethodLine}>Formulario</NavLink>
-          <NavLink icon={RiGitMergeLine}>Automação</NavLink>
+          <NavLink icon={RiInputMethodLine} href='forms'>Formulario</NavLink>
+          <NavLink icon={RiGitMergeLine} href='automation'>Automação</NavLink>
         </NavSection>
       </Stack>
     )
--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -10,6 +10,7 @@ import {
   VStack,
   Text,
 } from "@chakra-ui/react";
+import Link from "next/link";
 import Header from "../../components/Header";
 import { Sidebar } from "../../components/Siderbar/index";
 
@@ -21,7 +22,7 @@ export default function CreateUser() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p={["6","8"]}>
+        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
           <Heading size="lg" fontWeight="normal">
             Criar Usuario
           </Heading>
@@ -29,7 +30,7 @@ export default function CreateUser() {
           <Divider my="6" borderColor="gray.700" />
 
           <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing={["6","8"]} w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Box>
                 <Text>Nome completo</Text>
                 <Input name="name" label="Nome completo" />
@@ -41,26 +42,30 @@ export default function CreateUser() {
               </Box>
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth="240px" spacing={["6","8"]} w="100%">
-                <Box>
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+              <Box>
                 <Text>Senha</Text>
-              <Input name="password" type="password" label="Senha" />
-                </Box>
+                <Input name="password" type="password" label="Senha" />
+              </Box>
 
-                <Box>
+              <Box>
                 <Text>Confirmação da senha</Text>
-              <Input
-                name="password_confirmation"
-                type="password"
-                label="Confimação da senha"
-              />
-                </Box>
+                <Input
+                  name="password_confirmation"
+                  type="password"
+                  label="Confimação da senha"
+                />
+              </Box>
             </SimpleGrid>
           </VStack>
 
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Link href="/users" passHref>
+                <Button as="a" colorScheme="whiteAlpha">
+                  Cancelar
+                </Button>
+              </Link>
               <Button colorScheme="pink">Salvar</Button>
             </HStack>
           </Flex>
