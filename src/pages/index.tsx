2a4db13e2a5ff86cf87d33b697ba7c09ee8fe5af
Alisson Silva <alissonjosef@gmail.com>
2022-02-17 09:46:12 -0300

    [feat-05] feat: react hook form yup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
-import Head from "next/head";
 import { Button, Flex, Stack } from "@chakra-ui/react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { Input } from "../components/Form/Input";
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 
 type SignInformDara = {
   email: string
   password: string
 }
 
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required("E-mail obrigatorio").email("E-mail inválido"),
+  password: yup.string().required("Senha obrigatória"),
+})
+
 export default function SignIn() {
-  const { register, handleSubmit, formState } = useForm();
+  const { register, handleSubmit, formState, formState: { errors } } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  });
+ 
 
   const handleSignIn: SubmitHandler<SignInformDara>  = async (values) =>{
     await new Promise(resolve => setTimeout(resolve, 2000))
@@ -34,12 +43,14 @@ export default function SignIn() {
             name="email"
             type="email"
             label="E-mail"
+            error={errors.email}
             {...register("email", { required: true })}
           />
           <Input
             name="password"
             type="password"
             label="Senha"
+            error={errors.password}
             {...register("password", { required: true })}
           />
         </Stack>
